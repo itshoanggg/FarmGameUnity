66d8246b41c13b677c57ae3a2d37ae24e74d9587
itshoanggg <hoangdanghoctiengnhat@gmail.com>
2026-02-17 10:22:22 +0700

    Add login with firebase

--- Assets/Scripts/FirebaseLoginManager.cs
+++ Assets/Scripts/FirebaseLoginManager.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using Firebase.Auth;
 using Firebase.Extensions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FirebaseLoginManager : MonoBehaviour
 {
+    [Header("Register")]
     public InputField ipRegisterEmail;
     public InputField ipRegisterPassword;
-
     public Button btnRegister;
 
+    [Header("Login")]
+    public InputField ipLoginEmail;
+    public InputField ipLoginPassword;
+    public Button btnLogin;
+
     private FirebaseAuth auth;
+
+
+    [Header("Switch Form")]
+    public Button btnMoveToLogin;
+    public Button btnMoveToRegister; 
+
+    public GameObject registerForm;
+    public GameObject loginForm;
 
     private void Start()
     {
         auth = FirebaseAuth.DefaultInstance;
         btnRegister.onClick.AddListener(RegisterFirebase);
+        btnLogin.onClick.AddListener(LoginFirebase);
+
+        btnMoveToRegister.onClick.AddListener(SwitchForm);
+        btnMoveToLogin.onClick.AddListener(SwitchForm);
     }
 
     public void RegisterFirebase()
@@ -42,5 +60,37 @@ public class FirebaseLoginManager : MonoBehaviour
                 Debug.Log("User registered successfully"); 
             }
         });
+    }
+
+    public void LoginFirebase()
+    {
+        string email = ipLoginEmail.text;
+        string password = ipLoginPassword.text;
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("Login was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Login encountered an error: " + task.Exception);
+                return;
+            }
+            if(task.IsCompleted)
+            {
+                Debug.Log("User logged in successfully");
+                FirebaseUser user = task.Result.User;
+
+                SceneManager.LoadScene("PlayScene");
+            }
+        });
+    }
+
+    public void SwitchForm()
+    {
+        loginForm.SetActive(!loginForm.activeSelf);
+        registerForm.SetActive(!registerForm.activeSelf);
     }
 }
